f89c387170c975d3bbab473e0151a20e349c93da
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-03-11 23:55:44 -0500

    removed touch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -161,11 +161,5 @@ document.onkeyup = function (event) {
     
 };
 
-var clickEvent = (function() {
-    if ('ontouchstart' in document.documentElement === true)
-      return 'touchstart';
-    else
-      return 'click';
-  })();
 
 
